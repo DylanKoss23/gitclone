d86ae9f7ec4bd4ec9d5d498b9e1ae4c40038753d
Dolan <763408@ACDM.DS.SAIT.CA>
2024-01-16 13:28:09 -0700

    added the scripts.js for dailies day 2

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const person = {
+  firstName: "Dolan",
+  lastName: "Street",
+ };
+ 
+
+ const numbersArray = [3, 6, 12, 9];
+ function compareNumbers(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+     return "Error";
+  }
+  if (num1 > num2) {
+     return num1;
+  } else if (num1 < num2) {
+     return num2;
+  } else {
+     return "Both numbers are equal.";
+  }
+ }
+ 
+ 
+ function getFullName(user) {
+  if (user && user.firstName && user.lastName) {
+     return `${user.firstName} ${user.lastName}`;
+  } else {
+     return "Error: Invalid user object.";
+  }
+ }
+ 
+ console.log(compareNumbers(numbersArray[0], numbersArray[1]));
+ 
+ console.log(getFullName(person));
