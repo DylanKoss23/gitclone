e4d7bd1a9c203d3791c7012aac9b77b87ff29ffb
Dolan <763408@ACDM.DS.SAIT.CA>
2024-01-16 11:08:53 -0700

    added complex.js and conditionals.js

---
+++ complex.js
@@ -0,0 +1,44 @@
+const userOne = {
+  userID: 1234,
+  firstName: "John",
+  lastName: "Doe",
+  isActive: true,
+};
+const userTwo = {
+  userID: 1235,
+  firstName: "Jane",
+  lastName: "Doe",
+  isActive: true,
+};
+const userThree = {
+  userID: 1236,
+  firstName: "Johnny",
+  lastName: "Depp",
+  isActive: true,
+};
+
+//Const users = [userOne, userTwo, userThree];
+
+const users = [
+{
+  userID: 1234,
+  firstName: "John",
+  lastName: "Doe",
+  isActive: true,
+},
+{
+  userID: 1235,
+  firstName: "Jane",
+  lastName: "Doe",
+  isActive: true,
+},
+{
+  userID: 1236,
+  firstName: "Johnny",
+  lastName: "Depp",
+  isActive: true,
+},
+];
+
+
+console.log (users[1].firstName, users[2].lastName);
---
+++ conditionals.js
@@ -0,0 +1,19 @@
+const firstName = "Dylan";
+const lastName = "Koss";
+let isActive = true;
+const userID =2123
+
+
+//if (isActive) {
+ // console.log("The user is active.");
+ //} else {
+  //console.log("The user is not active.");
+ //};
+
+ if (isActive) {
+  console.log("The user is active.");
+ } else if (userID === 2123) {
+  console.log("The user's ID is 2123.");
+ } else {
+  console.log("The user is not active and the user's ID is not 2123.");
+ };
