6d374f9eaefcd4713aef4bb475960a71521566ed
Dolan <763408@ACDM.DS.SAIT.CA>
2024-01-16 10:33:19 -0700

    added java

---
+++ index.js
@@ -0,0 +1,16 @@
+const firstName = "Dylan";
+const lastName = "Koss";
+let isActive = true;
+const userID =2123
+
+console.log (`User ${userID} ${firstName} ${lastName} ${isActive}`);
+
+
+console.log(firstName);
+console.log(lastName);
+
+
+console.log(userID, isActive);
+
+
+console.log("User: " + firstName + " is " + userID);
